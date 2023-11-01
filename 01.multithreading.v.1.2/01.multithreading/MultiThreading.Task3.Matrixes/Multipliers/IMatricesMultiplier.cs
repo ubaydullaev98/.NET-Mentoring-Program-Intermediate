41c54bb30c88ab098a205d69594c7dab17542f19
Dilmurod Ubaydullaev <dilmurodubaydullaev22@gmail.com>
2023-11-01 22:29:32 +0500

    Task2 - done

--- 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task3.Matrixes/Multipliers/IMatricesMultiplier.cs
+++
@@ -1,9 +0,0 @@
-﻿using MultiThreading.Task3.MatrixMultiplier.Matrices;
-
-namespace MultiThreading.Task3.MatrixMultiplier.Multipliers
-{
-    public interface IMatricesMultiplier
-    {
-        IMatrix Multiply(IMatrix m1, IMatrix m2);
-    }
-}
